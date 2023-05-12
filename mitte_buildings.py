ed4c6da70f4cf6e6d60909aa8379e142edfb6127
mirkokemna <mirko.kemna@gmail.com>
2023-05-12 16:56:44 +0200

    seperate geoloading

--- mitte_buildings.py
+++ mitte_buildings.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas
 import geopandas
+from read_geodata import read_geodata
 
 ## get Mitte addresses
 # load address data base
@@ -10,35 +11,40 @@ mitte = address_data[address_data.iloc[:, 6] == 1].iloc[:, [9, 13]]
 # merge street & house number
 mitte = mitte[13] + ' ' + mitte[9].astype(str)
 
-# ## load PV potential data
-# solar_data = geopandas.read_file("./data/solar/gebaeude_pv_2021.gpkg").drop(columns='geometry')
-# solar_data.sort_values('sum_modarea', ascending=False, inplace=True)
-# solar_data["Mitte?"] = np.nan
-#
-# # iterate through buildings to check if in Mitte, limited to N largest for computational speed
-# N = 10000
-# count = 0
-# for index, row in solar_data.iterrows():
-#     count = count + 1
-#     street = row['lage']
-#     if not isinstance(street, str):
-#         continue
-#     switch = 0
-#     for i in range(len(street)):
-#         if not street[i].isnumeric():
-#             if switch == 1:
-#                 street = street[:i]
-#                 break
-#         else:
-#             switch = 1
-#
-#     if street in set(mitte):
-#         solar_data.loc[index, "Mitte?"] = 1
-#     else:
-#         solar_data.loc[index, "Mitte?"] = 0
-#
-#     if count == N:
-#         break
-#
-# # save dataset to Excel file
-# solar_data.iloc[:N, :].to_excel("solar_data_mitte.xlsx")
+## load PV potential data
+file = pandas.HDFStore('data/solar/solar_data.h5')
+try:
+    solar_data = file['solar_data']
+except:
+    read_geodata()
+    solar_data = file['solar_data']
+
+solar_data["Mitte?"] = np.nan
+
+# iterate through buildings to check if in Mitte, limited to N largest for computational speed
+N = 10000
+count = 0
+for index, row in solar_data.iterrows():
+    count = count + 1
+    street = row['lage']
+    if street == 'nan':
+        continue
+    switch = 0
+    for i in range(len(street)):
+        if not street[i].isnumeric():
+            if switch == 1:
+                street = street[:i]
+                break
+        else:
+            switch = 1
+
+    if street in set(mitte):
+        solar_data.loc[index, "Mitte?"] = 1
+    else:
+        solar_data.loc[index, "Mitte?"] = 0
+
+    if count == N:
+        break
+
+# save dataset to Excel file
+solar_data.iloc[:N, :].to_excel("solar_data_mitte.xlsx")
--- read_geodata.py
+++ read_geodata.py
@@ -1,11 +1,14 @@
+import pandas
 import geopandas
 
-data = geopandas.read_file("./data/dachflaechen_geeignet_pv_2021.gpkg")
 
-data["area"] = data.area
-
-data = data.drop(columns=['geb_id','geometry'])
-
-data.to_csv("area_rad")
-
-print(data)
+def read_geodata():
+    data = geopandas.read_file("./data/solar/gebaeude_pv_2021.gpkg")
+    data.sort_values('sum_modarea', ascending=False, inplace=True)
+    data["total area"] = data.area
+    data.drop(columns=['geometry'], inplace=True)
+    file = pandas.HDFStore('data/solar/solar_data.h5')
+    data['lage'] = data['lage'].astype(str)
+    data['geb_id'] = data['geb_id'].astype(str)
+    data['geb_funktion'] = data['geb_funktion'].astype(str)
+    file['solar_data'] = data
